3d7ace72bdfd11e273ccd86b9edd702ccb105e8c
atrevisanut <atrevisanut@gmail.com>
2016-05-23 09:23:05 -0600

    updated chatroom, moved

--- public/js/userHomeCtrl.js
+++ public/js/userHomeCtrl.js
@@ -1,4 +1,65 @@
-angular.module('skypeClone').controller('userHomeCtrl', function($scope, loginSvc, user, $state) {
+angular.module('skypeClone').controller('userHomeCtrl', function($scope, loginSvc, connectSvc, user, $state) {
+
+
+
+  $scope.interval;
+
+  $scope.getMessages = function(chatid){
+    connectSvc.getMessages($scope.chatid).then(function(response){
+    $scope.messages = response.messages;
+    });
+  };
+  $scope.postMessage = function(message){
+    connectSvc.postMessage(message).then(function(response){
+      $scope.messageNew = response;
+    });
+  };
+  $scope.newRoom = function(roomName){
+    connectSvc.newRoom(roomName).then(function(response){
+      $scope.roomNew = response;
+    });
+  };
+  $scope.getUsers = function(){
+    connectSvc.getCurrentUser().then(function(response){
+      $scope.user = response;
+    });
+  };
+  $scope.addToChat = function(fid, chatid){
+    connectSvc.addToChat(fid, chatid).then(function(response){
+      $scope.addedUserToChat = response;
+    });
+  };
+  $scope.addMsg = function(message, chatid){
+    connectSvc.addMsg(message, chatid).then(function(response){
+      $scope.chatid = chatid;
+      $scope.msgAdded = response;
+      $scope.getMessages(chatid);
+      $scope.newMsg='';
+    });
+  };
+
+  $scope.getChatRooms = function(chatid){
+    clearInterval($scope.interval);
+    $scope.chatid = chatid;
+  $scope.interval = setInterval($scope.getMessages, 500);
+    connectSvc.getChatRooms(chatid).then(function(response){
+      $scope.messages = response.messages;
+      $scope.roomNew = response;
+
+      // console.log($scope.messages)
+    });
+  };
+  $scope.$on("$destroy", function(){
+    clearInterval($scope.interval);
+  });
+
+  $scope.chatUserIn = function(){
+    connectSvc.chatUserIn().then(function(response){
+      $scope.allowedChats = response;
+
+    });
+  };
+
     $scope.username = user.username;
     console.log(user);
     $scope.logout = function() {
@@ -22,4 +83,6 @@ angular.module('skypeClone').controller('userHomeCtrl', function($scope, loginSv
         $scope.addedFriend = response;
       });
     };
+
+
 });
